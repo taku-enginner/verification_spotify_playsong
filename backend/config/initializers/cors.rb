a1047654ab9603ff766b2bc65475045f83d4b290
taka4655 <taku.enginner@gmail.com>
2025-02-20 19:03:36 +0900

    cors設定追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8000"
+    origins "localhost:8000", "https://verification-spotify-playsong.vercel.app/"
 
     resource "*",
       headers: :any,
